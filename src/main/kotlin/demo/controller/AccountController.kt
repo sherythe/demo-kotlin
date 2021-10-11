e32a24f3a37b707d87864181af006c0ceeb871a4
sherythe <88826801+sherythe@users.noreply.github.com>
2021-10-11 19:09:02 +0800

    Add a new endpoint to create new account

--- src/main/kotlin/demo/controller/AccountController.kt
+++ src/main/kotlin/demo/controller/AccountController.kt
@@ -1,10 +1,18 @@
 package demo.controller
 
+import demo.model.Account
 import demo.repository.AccountRepository
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/accounts")
 class AccountController(private val accountRepository: AccountRepository) {
+
+    @PostMapping()
+    fun createNewAccount(@RequestBody account: Account): Account =
+            accountRepository.save(account)
+
 }
